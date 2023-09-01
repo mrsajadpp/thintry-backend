6d7401905eccea6ec449f0228303e104efa1c1d5
mrsajadpp <saju4753@gmail.com>
2023-09-01 20:57:21 +0530

    new

--- index.js
+++ index.js
@@ -252,7 +252,12 @@ app.get('/fetch/tag/replies', async (req, res, next) => {});
 app.get('*', (req, res, next) => {
     console.log('Not Found!');
     res.json({ status: 404, message:'Api not found!' })
-})
+});
+
+app.post('*', (req, res, next) => {
+    console.log('Not Found!');
+    res.json({ status: 404, message:'Api not found!' })
+});
 
 app.listen(port, () => {
     console.log("Server started : " + port)
